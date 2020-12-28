5c96b7f9caf1e57fb8dfd142784591e3691fa927
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2020-12-29 00:18:27 +0100

    Implementation of several graph functions + degree distribution plot

--- functions/graph.py
+++ functions/graph.py
@@ -1,8 +1,10 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
+import matplotlib.pyplot as plt
 
 class Graph:
     def __init__(self):
         self.edges = defaultdict(set)
+        self.neighbours = defaultdict(set)
         self.nodes = set()
         self.__edges_num = 0
 
@@ -12,9 +14,12 @@ class Graph:
         self.__edges_num += 1
         self.edges[src].add(dst)
         self.nodes.update([src, dst])
+        self.neighbours[src].add(dst)
+        self.neighbours[dst].add(src)
         
     def is_directed(self):
-        pass
+        # TODO: when can we say a graph is directed?
+        return True
     
     def number_of_nodes(self):
         return len(self.nodes)
@@ -24,6 +29,31 @@ class Graph:
     
     def nodes_with_edges(self):
         return len(self.edges.keys())
+    
+    def density(self):
+        num_nodes = self.number_of_nodes()
+        num_edges = self.number_of_edges()
+        density = 2 * num_edges / (num_nodes * (num_nodes - 1))
+        if self.is_directed():
+            density = density / 2
+        return density
+    
+    def degree(self, node):
+        return len(self.neighbours[node])
+    
+    def degree_distribution_plot(self):
+        degrees = Counter([self.degree(n) for n in self.nodes])
+        x, y = zip(*degrees.items())
+        
+        plt.figure(figsize=(14, 6))
+        plt.xlabel('Degree')
+        plt.xscale('log')
+        plt.ylabel('Frequency')
+        plt.yscale('log')
+        plt.title('Degree distribution (logscale)')
+        plt.grid(alpha=0.5, zorder=1)
+        plt.scatter(x, y, marker='.', zorder=2)
+        plt.show()
     
     def __getitem__(self, src):
         if len(self.edges[src]) == 0:
