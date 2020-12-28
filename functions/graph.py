b5f14714df83780d02dcddb013fab930d26563a4
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2020-12-28 20:09:38 +0100

    Implementation of the graph

---
+++ functions/graph.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.edges = defaultdict(set)
+        self.nodes = set()
+        self.__edges_num = 0
+
+    def add_edge(self, src, dst):
+        if dst in self.edges[src]:
+            return
+        self.__edges_num += 1
+        self.edges[src].add(dst)
+        self.nodes.update([src, dst])
+        
+    def is_directed(self):
+        pass
+    
+    def number_of_nodes(self):
+        return len(self.nodes)
+    
+    def number_of_edges(self):
+        return self.__edges_num
+    
+    def __getitem__(self, src):
+        if len(self.edges[src]) == 0:
+            raise KeyError(src)
+        return list(self.edges[src])
--- functions/utils.py
+++ functions/utils.py
@@ -45,3 +45,9 @@ def unique_category(link_cat):
             new_file.write(cat + ' ' + str(link) + '\n')
             
     return link_cat
+
+
+import networkx as nx
+
+
+DG = nx.DiGraph()
